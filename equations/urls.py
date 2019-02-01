50a5a96206a6a3aa12edb462c686ef9d721e0653
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2019-02-01 08:12:38 +0100

    added index page

--- equations/urls.py
+++ equations/urls.py
@@ -19,6 +19,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html'), name="base"),
+    path('', TemplateView.as_view(template_name='index.html'), name="index"),
 
 ]
